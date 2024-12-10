70bebd77df3301519248083f3bc13dd9d9ba02cf
andrewdyer <andrewdyer@outlook.com>
2024-12-10 16:41:41 +0000

    feat: :sparkles: add dependency injection container
    Integrate a dependency injection container to manage application dependencies.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$container = (require_once __DIR__ . '/container.php')();
+
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
 
 return $app;
--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,44 +1,9 @@
 <?php
 
-$container = $app->getContainer();
+use DI\Container;
 
-$container['foundHandler'] = function () {
-    return new Slim\Handlers\Strategies\RequestResponseArgs();
-};
+return function() {
+    $container = new Container();
 
-$container['logger'] = function ($container) {
-    $config = $container->get('settings')->get('logger');
-
-    $formatter = new Monolog\Formatter\LineFormatter(
-        $config['formatter']['format'] . "\n",
-        $config['formatter']['dateFormat'],
-        $config['formatter']['allowInlineLineBreaks'],
-        $config['formatter']['ignoreEmptyContextAndExtra']
-    );
-
-    $handler = new Monolog\Handler\StreamHandler(
-        base_path('storage/logs/app.log'),
-        $config['handler']['level']
-    );
-
-    $handler->setFormatter($formatter);
-
-    $logger = new Monolog\Logger($config['name']);
-    $logger->pushHandler($handler);
-
-    return $logger;
-};
-
-$container['view'] = function ($container) {
-    $config = $container->get('settings')->get('view');
-
-    $view = new Slim\Views\Twig(base_path('resources/views'), [
-        'cache' => $config['cache'],
-    ]);
-
-    $router = $container->get('router');
-    $uri = Slim\Http\Uri::createFromEnvironment(new Slim\Http\Environment($_SERVER));
-    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
-
-    return $view;
+    return $container;
 };
