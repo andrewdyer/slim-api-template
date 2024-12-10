e88255f52abe5f19f4ea2d00a13b8626033b40fd
andrewdyer <andrewdyer@outlook.com>
2024-12-10 17:31:47 +0000

    feat: :sparkles: add logging
    Added logging configuration and integrate Monolog for enhanced logging.

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,6 +1,9 @@
 <?php
 
 use DI\Container;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
 
 return function() {
     $container = new Container();
@@ -18,7 +21,32 @@ return function() {
                 'collation' => get_env('DB_COLLATION'),
                 'prefix'    => get_env('DB_PREFIX'),
             ],
+            'logger' => [
+                'name'        => get_env('LOG_NAME', 'app'),
+                'path'        => root_path('storage/logs/app.log'),
+                'level'       => Logger::toMonologLevel(get_env('LOG_LEVEL', 'DEBUG')),
+                'max_files'   => (int)get_env('LOG_MAX_FILES', 30),
+                'log_format'  => get_env('LOG_FORMAT', "[%datetime%] %level_name%: %message% %context% %extra%\n"),
+                'date_format' => get_env('LOG_DATE_FORMAT', 'Y-m-d H:i:s'),
+            ],
         ];
+    });
+
+    $container->set(Logger::class, function($container) {
+        $settings = $container->get('settings')['logger'];
+
+        $logger = new Logger($settings['name']);
+        $handler = new RotatingFileHandler($settings['path'], $settings['max_files'], $settings['level']);
+
+        $formatter = new LineFormatter(
+            "[%datetime%] %level_name%: %message% %context% %extra%\n",
+            'Y-m-d H:i:s'
+        );
+        $handler->setFormatter($formatter);
+
+        $logger->pushHandler($handler);
+
+        return $logger;
     });
 
     return $container;
