44d959ca05ec91b68d0587903196bd05a1ccfbd9
Andrew Dyer <andrewdyer@outlook.com>
2025-04-22 18:19:45 +0100

    docs: :books: add documentation for controller registration in controllers.php

--- bootstrap/controllers.php
+++ bootstrap/controllers.php
@@ -4,6 +4,21 @@ use App\Http\Controllers\IndexController;
 use DI\Container;
 use Slim\Views\Twig;
 
+/**
+ * CONTROLLERS
+ *
+ * Registers controller classes in the dependency injection container.
+ * Controllers are responsible for handling incoming HTTP requests and
+ * returning appropriate responses.
+ *
+ * Each controller should be defined in `src/app/Http/Controllers` and
+ * registered here with its required dependencies.
+ *
+ * To register a new controller:
+ * $container->set(MyController::class, function () use ($container) {
+ *     return new MyController($container->get(MyService::class));
+ * });
+ */
 return function(Container $container) {
     $container->set(IndexController::class, function() use ($container) {
         return new IndexController($container->get(Twig::class));
