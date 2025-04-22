f680a764d3b8fbf2adf55066542b366a51a296fa
Andrew Dyer <andrewdyer@outlook.com>
2025-04-23 00:14:57 +0100

    refactor: :hammer: remove LoggerService and TwigService
    Integrated functionality directly in services.php

--- bootstrap/services.php
+++ bootstrap/services.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * Registers shared services and dependencies in the dependency
  * injection container.
  *
@@ -8,20 +8,16 @@
  * external APIs, or any custom service classes used throughout
  * the application.
  *
- * Each service should be defined in `app/Services` and registered
- * here with its required configuration or dependencies, often
- * sourced from the settings defined in `settings.php`.
- *
  * To register a new service:
- * $container->set(MyService::class, function () use ($container) {
+ * $container->set(MyService::class, function($container) {
  *     $config = $container->get('settings')['my_service'];
  *     return new MyService($config);
  * });
  */
 
-use App\Services\LoggerService;
-use App\Services\TwigService;
 use DI\Container;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
 use Slim\Views\Twig;
 
@@ -29,12 +25,32 @@ return function(Container $container) {
     $container->set(Logger::class, function($container) {
         $settings = $container->get('settings')['logger'];
 
-        return (new LoggerService($settings))();
+        $logger = new Logger($settings['name']);
+
+        $handler = new RotatingFileHandler(
+            root_path('storage/logs/app.log'),
+            $settings['max_files'],
+            $settings['level']
+        );
+
+        $formatter = new LineFormatter(
+            "[%datetime%] %level_name%: %message% %context% %extra%\n",
+            'Y-m-d H:i:s'
+        );
+
+        $handler->setFormatter($formatter);
+
+        $logger->pushHandler($handler);
+
+        return $logger;
     });
 
     $container->set(Twig::class, function($container) {
         $settings = $container->get('settings')['view'];
 
-        return (new TwigService($settings))();
+        return Twig::create(
+            root_path('resources/views'),
+            ['cache' => $settings['cache']]
+        );
     });
 };
