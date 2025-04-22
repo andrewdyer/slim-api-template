2386e708aff698af101c5ce1c0eb081f2aea2dbf
Andrew Dyer <andrewdyer@outlook.com>
2025-04-22 19:05:33 +0100

    docs: :books: add service registration documentation in services.php

--- bootstrap/services.php
+++ bootstrap/services.php
@@ -6,15 +6,30 @@ use DI\Container;
 use Monolog\Logger;
 use Slim\Views\Twig;
 
+/*
+ * SERVICES
+ *
+ * Registers shared services and dependencies in the dependency injection container.
+ * These may include loggers, templating engines, database clients, external APIs,
+ * or any custom service classes used throughout the application.
+ * 
+ * Each service should be defined in `app/Services` and registered here with its
+ * required configuration or dependencies, often sourced from the settings defined
+ * in `settings.php`.
+ *
+ * To register a new service:
+ * $container->set(MyService::class, function () use ($container) {
+ *     $config = $container->get('settings')['my_service'];
+ *     return new MyService($config);
+ * });
+ */
 return function(Container $container) {
-    // Register the Logger service
     $container->set(Logger::class, function($container) {
         $settings = $container->get('settings')['logger'];
 
         return (new LoggerService($settings))();
     });
 
-    // Register the Twig service
     $container->set(Twig::class, function($container) {
         $settings = $container->get('settings')['view'];
 
