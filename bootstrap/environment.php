f33006d2eddafe36e96957dba1dfccfcde526d14
Andrew Dyer <andrewdyer@outlook.com>
2025-05-24 15:40:01 +0100

    refactor: :hammer: enhance environment variable loading with optional filename parameter and improved documentation

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -1,23 +1,28 @@
 <?php
 
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
+
 /**
- * Loads environment variables from a `.env` file into the application.
+ * Loads environment variables from a specified file into the application.
  *
  * Environment variables are used to configure key parts of the application,
  * such as database connections, logging, and external services.
  *
- * The `.env` file is only loaded when the application environment `APP_ENV`
- * is set to 'local' or is not defined. In all other environments, it's assumed
- * that environment variables are already set at the system level.
+ * The environment file is loaded only when the application environment `APP_ENV`
+ * is not set. In all other environments, it assumes environment variables are
+ * already set externally (e.g., in production).
+ *
+ * @param string|null $filename The name of the environment file to load. Defaults to '.env'.
+ *
+ * @return void
+ *
+ * @throws InvalidPathException If the specified env file is not found.
  */
-
-use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
-
-return function() {
-    if (!get_env('APP_ENV') || get_env('APP_ENV') === 'local') {
+return function(?string $filename = '.env'): void {
+    if (!get_env('APP_ENV')) {
         try {
-            $dotenv = Dotenv::createImmutable(root_path('/'));
+            $dotenv = Dotenv::createImmutable(root_path('/'), $filename);
             $dotenv->load();
         } catch (InvalidPathException $ex) {
             exit($ex->getMessage());
