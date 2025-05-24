4f25a7db4af313575ac13b4fa603ab1ff22a990f
Andrew Dyer <andrewdyer@outlook.com>
2025-05-24 14:39:09 +0100

    refactor: :hammer: simplify environment variable retrieval by removing boolean and integer helper functions

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -12,20 +12,4 @@ final class HelpersTest extends TestCase
         $this->assertEquals('value', get_env('TEST_KEY'));
         $this->assertEquals('default', get_env('NON_EXISTENT_KEY', 'default'));
     }
-
-    public function testGetEnvBool()
-    {
-        $_ENV['TEST_BOOL_TRUE'] = 'true';
-        $_ENV['TEST_BOOL_FALSE'] = 'false';
-        $this->assertTrue(get_env_bool('TEST_BOOL_TRUE'));
-        $this->assertFalse(get_env_bool('TEST_BOOL_FALSE'));
-        $this->assertFalse(get_env_bool('NON_EXISTENT_BOOL', false));
-    }
-
-    public function testGetEnvInt()
-    {
-        $_ENV['TEST_INT'] = '123';
-        $this->assertEquals(123, get_env_int('TEST_INT'));
-        $this->assertEquals(0, get_env_int('NON_EXISTENT_INT'));
-    }
 }
