3eaf16900e78fdf8a16884e2223909ee44a2a17f
Andrew Dyer <andrewdyer@outlook.com>
2025-05-24 18:27:48 +0100

    refactor: :hammer: move User model to App\Models namespace and update references

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
