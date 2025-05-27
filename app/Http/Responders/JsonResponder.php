e5e35d816284cc646040c0d3bfe7d6dc03a384ee
Andrew Dyer <andrewdyer@outlook.com>
2025-05-27 12:30:07 +0100

    refactor: :hammer: change JsonResponder to readonly class and remove unnecessary interface implementation

--- app/Http/Responders/JsonResponder.php
+++ app/Http/Responders/JsonResponder.php
@@ -6,9 +6,8 @@ namespace App\Http\Responders;
 
 use Psr\Http\Message\ResponseInterface;
 
-class JsonResponder implements ResponderInterface
+readonly class JsonResponder
 {
-    #[\Override]
     public function respond(ResponseInterface $response, mixed $data, int $status = 200): ResponseInterface
     {
         $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
