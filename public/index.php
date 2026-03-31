096399ff3253f64d3693a5a92630865fbb5080a0
Andrew Dyer <andrewdyer@outlook.com>
2026-03-31 10:55:01 +0100

    feat: :sparkles: implement CORS response handling
    Add CorsResponseEmitter to handle CORS responses in the app.

--- public/index.php
+++ public/index.php
@@ -2,10 +2,18 @@
 
 declare(strict_types=1);
 
+use AndrewDyer\Slim\CorsResponseEmitter;
+use Slim\Factory\ServerRequestCreatorFactory;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $appFactory = require __DIR__ . '/../bootstrap/app.php';
 
 $app = $appFactory();
 
-$app->run();
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+
+$response = $app->handle($request);
+$corsResponseEmitter = new CorsResponseEmitter();
+$corsResponseEmitter->emit($response);
