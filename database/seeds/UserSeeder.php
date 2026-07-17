ab2d92e3a16f93704953c0c9641a089fe0a0f294
Andrew Dyer <andrewdyer@outlook.com>
2026-07-17 13:19:53 +0100

    feat: add UserSeeder
    Added UserSeeder to populate users table with example data

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Seed\AbstractSeed;
+
+/**
+ * Seeds the users table with an example user.
+ */
+final class UserSeeder extends AbstractSeed
+{
+    /**
+     * Inserts the example user.
+     */
+    public function run(): void
+    {
+        $this->table('users')
+            ->insert([
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john.doe@example.com',
+            ])
+            ->saveData();
+    }
+}
