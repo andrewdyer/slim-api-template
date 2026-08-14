55f4c2ade5ad50cde4cf05754a01526e2e9157de
Andrew Dyer <andrewdyer@outlook.com>
2026-08-14 12:58:47 +0100

    feat: add role repository binding to the container

--- bootstrap/repositories.php
+++ bootstrap/repositories.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use App\Domain\Repositories\PermissionRepositoryInterface;
+use App\Domain\Repositories\RoleRepositoryInterface;
 use App\Domain\Repositories\UserRepositoryInterface;
 use App\Infrastructure\Persistence\Repositories\EloquentPermissionRepository;
+use App\Infrastructure\Persistence\Repositories\EloquentRoleRepository;
 use App\Infrastructure\Persistence\Repositories\EloquentUserRepository;
 use DI\ContainerBuilder;
 
@@ -15,6 +17,9 @@ return static function(ContainerBuilder $containerBuilder): void {
     $containerBuilder->addDefinitions([
         PermissionRepositoryInterface::class => static function(): EloquentPermissionRepository {
             return new EloquentPermissionRepository();
+        },
+        RoleRepositoryInterface::class => static function(): EloquentRoleRepository {
+            return new EloquentRoleRepository();
         },
         UserRepositoryInterface::class => static function(): EloquentUserRepository {
             return new EloquentUserRepository();
