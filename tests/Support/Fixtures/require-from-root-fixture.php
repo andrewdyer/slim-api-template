0dff296ee7213a2976e0e633dde009aaa8bfa239
Andrew Dyer <andrewdyer@outlook.com>
2026-08-14 21:07:42 +0100

    test: update comment for HelpersRequireFromRootTest in require-from-root-fixture.php

--- tests/Support/Fixtures/require-from-root-fixture.php
+++ tests/Support/Fixtures/require-from-root-fixture.php
@@ -2,5 +2,5 @@
 
 declare(strict_types=1);
 
-// Required by HelpersGetEnvTest to assert require_from_root() returns the required file's value.
+// Required by HelpersRequireFromRootTest to assert require_from_root() returns the required file's value.
 return ['fixture' => true];
